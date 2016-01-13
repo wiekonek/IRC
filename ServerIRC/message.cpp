2a92b61ee0fb66852ed4770d28ab6a87fedb7f31
jjax <jax1994@gmail.com>
2016-01-14 00:30:30 +0100

    message add() - adds content to message getChar() - returns json in char[]

--- ServerIRC/message.cpp
+++ ServerIRC/message.cpp
@@ -1,7 +1,37 @@
 #include "message.h"
+//qDebug(qPrintable(answear.toString()));
+
 
 Message::Message()
 {
+    container = new QJsonObject();
+}
 
+void Message::add(QString key, QString value)
+{
+    QJsonValue *json_value = new QJsonValue(value);
+    container->insert(key, *json_value);
+}
+
+QString Message::getValue(QString key)
+{
+    QJsonValue answear = container->value(key);
+    return answear.toString();
+}
+
+QByteArray Message::toByte()
+{
+    QJsonDocument *json_document = new QJsonDocument(*container);
+    return json_document->toJson();
+}
+
+char* Message::toChar()
+{
+    return toByte().data();
+}
+
+void Message::printAll()
+{
+    QDebug(toByte());
 }
 
--- ServerIRC/message.h
+++ ServerIRC/message.h
@@ -2,13 +2,27 @@
 #define MESSAGE_H
 
 #include "socketmanager.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QFile>
 
 class Message
 {
 public:
     Message();
+    void add(QString key, QString value);
+    QString getValue(QString key);
+    QByteArray toByte();
+    char* toChar();
+    void printAll();
 
     struct sockaddr_in addres;
+
+private:
+    QJsonObject *container;
+
 
 };
 
